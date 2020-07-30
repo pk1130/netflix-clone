e47d5997a32e020b9fb1aa0ab452de33025b9f2b
pk1130 <pk1130@umd.edu>
2020-07-30 12:12:15 +0300

    Created API requests and baseURL

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "f5a35d9f517b2e1268923f5d4e8b216d";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
